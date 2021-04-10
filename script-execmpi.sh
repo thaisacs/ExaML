835b70ea68a643e585d13cc7451b46b8390ad2f8
Thais Camacho <thaiscamachoo@gmail.com>
2021-04-10 12:54:25 -0300

    up

--- script-execmpi.sh
+++ script-execmpi.sh
@@ -1,5 +1,18 @@
+DATASET=exp-results
+rm -rf $DATASET
+mkdir $DATASET
+
 cd parser
 ./parse-examl -m DNA -s ../testData/49 -q ../testData/49.model -n 49
-
 cd ..
-mpirun -np $1 ./examl/examl -t testData/49.tree -m GAMMA -n T$1 -s parser/49.binary
+
+APP=
+
+sudo perf stat -o $DATASET/perf-examl.out ./examl/examl -t testData/49.tree -m GAMMA -n T1 -s parser/49.binary
+/usr/bin/time -o $DATASET/time-examl.out -v ./examl/examl -t testData/49.tree -m GAMMA -n T2 -s parser/49.binary
+ltrace -o $DATASET/mpi-examl.out ./examl/examl -t testData/49.tree -m GAMMA -n T3 -s parser/49.binary
+
+cat $DATASET/mpi-examl.out | grep "MPI" > $DATASET/mpi2-examl.out
+rm $DATASET/mpi-examl.out
+
+#mpirun -np $1 ./examl/examl -t testData/49.tree -m GAMMA -n T$1 -s parser/49.binary
