f309e70ccc44609ee7fa2044c57b222cccefd1cc
Thais Camacho <thaiscamachoo@gmail.com>
2021-05-06 21:20:32 -0300

    up

--- build_scripts/run_clean.sh
+++ build_scripts/run_clean.sh
@@ -1,5 +1,8 @@
 cd ..
-rm examl/examl
-rm parser/49.binary
-rm parser/RAxML_info.49
+rm -f examl/examl
+rm -f parser/49.binary
+rm -f parser/RAxML_info.49
+rm -f parser/140.binary
+rm -f parser/RAxML_info.140
+rm -f ExaML_*
 
