d4024bdadb1e1f46cb986d3a6f05118624a55cff
Thais <thaiscamachoo@gmail.com>
2021-06-19 12:55:56 -0300

    up

--- build_scripts/run_build.sh
+++ build_scripts/run_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ../parser
 make -f Makefile.SSE3.gcc
+./parse-examl -m DNA -s ../testData/49 -q ../testData/49.model -n 49
+./parse-examl -m DNA -s ../testData/140 -q ../testData/140.model -n 140
 cd ../examl
 make -f Makefile.SSE3.gcc
--- exec_scripts/run_exec.sh
+++ exec_scripts/run_exec.sh
@@ -4,13 +4,10 @@ DATASET=$PWD/$1
 NAME=$(date +"%m-%d-%y-%T")
 mkdir $DATASET
 cd ..
-examl_A="./examl/examl -t testData/49.tree -m GAMMA -n T1 -s parser/49.binary"
-examl_B="./examl/examl -t testData/140.tree -m GAMMA -n T2 -s parser/140.binary"
-cd parser
-./parse-examl -m DNA -s ../testData/49 -q ../testData/49.model -n 49
-./parse-examl -m DNA -s ../testData/140 -q ../testData/140.model -n 140
-cd ..
-mpirun -n $2 --hostfile $HOSTFILE $examl_A > $DATASET/$NAME-examl-A.out 2> $DATASET/$NAME-examl-A.err
-mpirun -n $2 --hostfile $HOSTFILE $examl_B > $DATASET/$NAME-examl-B.out 2> $DATASET/$NAME-examl-B.err
+examl_a_app="./examl/examl -t testData/49.tree -m GAMMA -n T1 -s parser/49.binary"
+examl_b_app="./examl/examl -t testData/140.tree -m GAMMA -n T2 -s parser/140.binary"
+# examl
+#mpirun -n $2 --hostfile $HOSTFILE $examl_a_app > $DATASET/$NAME-examl-A.out 2> $DATASET/$NAME-examl-A.err
+mpirun -n $2 --hostfile $HOSTFILE $examl_b_app > $DATASET/$NAME-examl-B.out 2> $DATASET/$NAME-examl-B.err
 rm -f *T1*
 rm -f *T2*
