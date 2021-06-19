c27d39653beae7c70a4812691b68533976623dda
Thais <thaiscamachoo@gmail.com>
2021-06-19 16:22:42 -0300

    up

--- exec_scripts/run_exec.sh
+++ exec_scripts/run_exec.sh
@@ -7,7 +7,7 @@ cd ..
 examl_a_app="./examl/examl -t testData/49.tree -m GAMMA -n T1 -s parser/49.binary"
 examl_b_app="./examl/examl -t testData/140.tree -m GAMMA -n T2 -s parser/140.binary"
 # examl
-#mpirun -n $2 --hostfile $HOSTFILE $examl_a_app > $DATASET/$NAME-examl-A.out 2> $DATASET/$NAME-examl-A.err
+mpirun -n $2 --hostfile $HOSTFILE $examl_a_app > $DATASET/$NAME-examl-A.out 2> $DATASET/$NAME-examl-A.err
 mpirun -n $2 --hostfile $HOSTFILE $examl_b_app > $DATASET/$NAME-examl-B.out 2> $DATASET/$NAME-examl-B.err
 rm -f *T1*
 rm -f *T2*
